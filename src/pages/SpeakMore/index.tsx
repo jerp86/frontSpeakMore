a5882382b7d1ac3620be470bbf98a24468878fc4
Jose Eduardo Rodrigues Pinto (jerp86) <jerp4@hotmail.com>
2021-01-10 13:50:53 -0300

    add SpeakMore page

--- src/pages/SpeakMore/index.tsx
+++ src/pages/SpeakMore/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
-const SpeakMore: React.FC = () => {
-  return <h1>SpeakMore</h1>;
-};
+import { Container, Content, First } from './styles';
+
+import Form from '../../components/Form';
+import Footer from '../../components/Footer';
+
+const SpeakMore: React.FC = () => (
+  <Container>
+    <Content>
+      <First>
+        <h2>
+          Conheça o <br /> FaleMais VxTel©
+        </h2>
+        <p>Compare o custo de suas ligações</p>
+      </First>
+
+      <Form />
+    </Content>
+    <Footer />
+  </Container>
+);
 
 export default SpeakMore;
---
+++ src/pages/SpeakMore/styles.ts
@@ -0,0 +1,91 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 60vw;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media (max-width: 500px) {
+    width: 100%;
+    padding: 1rem;
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  position: relative;
+
+  width: 100%;
+  height: 100%;
+
+  background-color: #f4f2ed;
+  border-radius: 1rem;
+
+  @media (max-width: 500px) {
+    flex-direction: column;
+    width: 100%;
+    padding: 0%;
+  }
+
+  ::before {
+    content: '';
+    position: absolute;
+    left: 0;
+
+    width: 50%;
+    height: 100%;
+
+    background-color: #b92c2c;
+    border-top-left-radius: 1rem;
+    border-bottom-left-radius: 1rem;
+
+    @media (max-width: 500px) {
+      position: relative;
+    }
+  }
+`;
+
+export const First = styled.div`
+  z-index: 10;
+  width: 50%;
+  padding: 0.5rem;
+
+  text-align: center;
+
+  background-color: #b92c2c;
+  color: #f4f2ed;
+
+  h2 {
+    font-weight: bold;
+    font-size: 1.75rem;
+    line-height: 1.875rem;
+  }
+
+  p {
+    font-size: 0.875rem;
+    line-height: 1rem;
+    padding: 1rem 0;
+  }
+
+  @media (max-width: 768px) {
+    h2 {
+      font-size: 1.625rem;
+      line-height: 1.75rem;
+    }
+
+    p {
+      font-size: 0.75rem;
+      line-height: 0.875rem;
+    }
+  }
+
+  @media (max-width: 500px) {
+    display: none;
+  }
+`;
