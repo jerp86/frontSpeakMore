aa99c658680bf592d8997ae7fe31a3102f1b627f
Jose Eduardo Rodrigues Pinto (jerp86) <jerp4@hotmail.com>
2021-01-12 07:09:21 -0300

    refactory Form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,6 +1,10 @@
+/* eslint-disable consistent-return */
 import React, { useCallback, useEffect, useState } from 'react';
 
 import { Second, ContentForm, ButtonList } from './styles';
+
+import { useCalc } from '../../hooks/calculate';
+import api from '../../services/api';
 
 interface IDddProps {
   id: string;
@@ -24,30 +28,26 @@ interface IPlanProps {
 const Form: React.FC = () => {
   const [ddds, setDdds] = useState<IDddProps[]>([]);
   const [originSelected, setOriginSelected] = useState(0);
-  const [destinySelected, setDestinySelected] = useState(0);
+  const [destinySelected, setDestinySelected] = useState('');
+  const [timeSelected, setTimeSelected] = useState(0);
+  const [planSelected, setPlanSelected] = useState(0);
   const [products, setProducts] = useState<IProductProps[]>([]);
   const [plans, setPlans] = useState<IPlanProps[]>([]);
 
+  const { comparativo, getPlan } = useCalc();
+
   useEffect(() => {
-    const url = 'http:////192.168.15.13:3334';
+    api.get(`/ddds`).then(response => {
+      setDdds(response.data.map((res: IDddProps) => res));
+    });
 
-    fetch(`${url}/ddds`)
-      .then(resp => resp.json())
-      .then(data => {
-        setDdds(data.map((res: IDddProps) => res));
-      });
+    api.get(`/products`).then(response => {
+      setProducts(response.data.map((res: IProductProps) => res));
+    });
 
-    fetch(`${url}/products`)
-      .then(resp => resp.json())
-      .then(data => {
-        setProducts(data.map((res: IProductProps) => res));
-      });
-
-    fetch(`${url}/plans`)
-      .then(resp => resp.json())
-      .then(data => {
-        setPlans(data.map((res: IPlanProps) => res));
-      });
+    api.get(`/plans`).then(response => {
+      setPlans(response.data.map((res: IPlanProps) => res));
+    });
   }, []);
 
   const getDestiny = useCallback(
@@ -55,7 +55,7 @@ const Form: React.FC = () => {
       const plansFiltered = plans.filter(p => p.origin === Number(codeFind));
 
       return plansFiltered.map(({ id, destiny, value }) => (
-        <option key={id} value={destiny}>
+        <option key={id} value={id}>
           {destiny} -&gt; R$ {value}/min
         </option>
       ));
@@ -63,68 +63,87 @@ const Form: React.FC = () => {
     [plans],
   );
 
+  const clicou = useCallback(async () => {
+    const { value } = await getPlan(destinySelected);
+
+    if (!value || !timeSelected || !planSelected) {
+      return;
+    }
+
+    comparativo(value, timeSelected, planSelected);
+  }, [getPlan, destinySelected, comparativo, timeSelected, planSelected]);
+
   return (
-    <Second>
-      <ContentForm>
-        <label htmlFor="origin">
-          Origem:
-          <select
-            id="origin"
-            onChange={e => setOriginSelected(Number(e.target.value))}
-            required
-          >
-            <option hidden>Escolha a Origem</option>
-            {ddds &&
-              ddds.map(({ id, code, description }) => (
-                <option key={id} value={code}>
-                  {code} -&gt; {description}
-                </option>
-              ))}
-          </select>
-        </label>
+    <>
+      <Second>
+        <ContentForm>
+          <label htmlFor="origin">
+            Origem:
+            <select
+              id="origin"
+              onChange={e => setOriginSelected(Number(e.target.value))}
+              required
+            >
+              <option hidden>Escolha a Origem</option>
+              {ddds &&
+                ddds.map(({ id, code, description }) => (
+                  <option key={id} value={code}>
+                    {code} -&gt; {description}
+                  </option>
+                ))}
+            </select>
+          </label>
 
-        <label htmlFor="destiny">
-          Destino:
-          <select
-            id="destiny"
-            onChange={e => setDestinySelected(Number(e.target.value))}
-            required
-          >
-            <option hidden>Escolha o Destino</option>
-            {originSelected && getDestiny(originSelected)}
-          </select>
-        </label>
+          <label htmlFor="destiny">
+            Destino:
+            <select
+              id="destiny"
+              onChange={e => setDestinySelected(e.target.value)}
+              required
+            >
+              <option hidden>Escolha o Destino</option>
+              {originSelected && getDestiny(originSelected)}
+            </select>
+          </label>
 
-        <label htmlFor="plan">
-          Plano:
-          <select id="plan" required>
-            <option hidden>Escolha o Plano</option>
-            {destinySelected &&
-              products &&
-              products.map(({ id, value, description }) => (
-                <option key={id} value={value}>
-                  {description}
-                </option>
-              ))}
-          </select>
-        </label>
+          <label htmlFor="plan">
+            Plano:
+            <select
+              id="plan"
+              value={planSelected}
+              onChange={e => setPlanSelected(Number(e.target.value))}
+              required
+            >
+              <option hidden>Escolha o Plano</option>
+              {destinySelected &&
+                products &&
+                products.map(({ id, value, description }) => (
+                  <option key={id} value={value}>
+                    {description}
+                  </option>
+                ))}
+            </select>
+          </label>
 
-        <label htmlFor="origin">
-          Tempo:
-          <input
-            type="number"
-            min="1"
-            max="250"
-            name="time"
-            id="time"
-            placeholder="Informe os minutos..."
-            autoComplete="new-password"
-          />
-        </label>
-      </ContentForm>
+          <label htmlFor="origin">
+            Tempo:
+            <input
+              type="number"
+              min="1"
+              max="250"
+              name="time"
+              id="time"
+              value={timeSelected}
+              onChange={e => setTimeSelected(Number(e.target.value))}
+              placeholder="Informe os minutos..."
+              autoComplete="new-password"
+            />
+          </label>
+        </ContentForm>
 
-      <ButtonList>Calcular</ButtonList>
-    </Second>
+        <ButtonList onClick={clicou}>Calcular</ButtonList>
+      </Second>
+    </>
   );
 };
 
